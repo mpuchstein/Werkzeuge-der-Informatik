782581b22c48eb17890ae11bfbf18f2970350873
Kiphboy <Philipp.Seibel@outlook.de>
2020-11-17 09:35:32 +0100

    Revert "Update Main.java"
    This reverts commit 38e6b1118fd94c795a53bfd0f64dde8861480751.

--- src/main/java/de/unitrier/st/Main.java
+++ src/main/java/de/unitrier/st/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         HTTPCall call = new HTTPCall();
-System.out.println("hi");
         System.out.println(call.send("http://www.mocky.io/v2/5a9ce37b3100004f00ab5154"));
     }
 
